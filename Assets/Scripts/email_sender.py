bdbcb021e19c1635f543ed101aa3bfe316945bf9
j102398 <147440159+j102398@users.noreply.github.com>
2025-10-18 22:59:02 +0100

    Refactor send_password to generate OTP internally
    Removed otp parameter from send_password function and generated otp internally.

--- Assets/Scripts/email_sender.py
+++ Assets/Scripts/email_sender.py
@@ -2,9 +2,10 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 from email.mime.text import MIMEText  # You'll need this for HTML
+import random
 
-
-def send_password(otp, recipient_email):
+def send_password( recipient_email):
+    otp = random.randint(100000,999999)
     # Your credentials
     email_sender = 'smtp41290@gmail.com'
     email_app_password = 'tmtq ukqn dsdr ozro'
@@ -49,6 +50,11 @@ def send_password(otp, recipient_email):
     # Send the email and close the connection
     smtp_server.sendmail(email_sender, recipient_email, email.as_string())
     smtp_server.quit()
+    return otp #this returns the otp to the c# program
+
 
 # Example usage (if you want to test it):
-send_password(123456, 'your_email')
+send_password( 'your_email')
+
+
+
